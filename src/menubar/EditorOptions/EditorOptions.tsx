11b5be8ad922520627ffe76c055c1d36e5dcf823
Sid Sun <sid@dronk.dev>
2023-09-03 20:55:13 +0100

    redesign menubar
    Signed-off-by: Sid Sun <sid@dronk.dev>

--- src/menubar/EditorOptions/EditorOptions.tsx
+++ src/menubar/EditorOptions/EditorOptions.tsx
@@ -7,10 +7,10 @@ function EditorOptions() {
   let { wrapLines, setWrapLines } = useEditorStore(state => ({ wrapLines: state.wrapLines, setWrapLines: state.setWrapLines }))
 
   return (<React.Fragment>
-    <h4 className="font-mono text-center text-xl pb-4 mt-4">Editor Options</h4>
+    <h4 className="font-mono text-xl pb-1 mt-6">Preferences</h4>
     <div className="items-center">
       <label htmlFor="theme" className="text-center font-mono pb-2">Theme:</label>
-      <select id="theme" value={theme} onChange={e => setTheme(e.target.value)} className="form-select h-8 appearance-none block w-full px-1 py-0.5 text-base font-mono text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out mx-0 mt-0.5 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+      <select id="theme" value={theme} onChange={e => setTheme(e.target.value)} className="form-select h-10 appearance-none block w-full px-1 py-0.5 text-base font-mono text-gray-700 bg-pink-50 bg-clip-padding bg-no-repeat border border-solid border-pink-50 rounded transition ease-in-out mx-0 mt-0.5 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
         <option value="aura">Aura Dark</option>
         <option value="github-dark">GitHub Dark</option>
         <option value="bbedit">BBEdit Light</option>
@@ -19,10 +19,10 @@ function EditorOptions() {
     <div className="mt-2">
       <label htmlFor="fontSize" className="text-center font-mono">Font Size</label>
       <br />
-      <input onChange={e => setFontSize(parseInt(e.target.value))} value={fontSize} type="range" id="fontSize" max="26" min="10" step="1" className="w-full h-8 form-check-input" />
+      <input onChange={e => setFontSize(parseInt(e.target.value))} value={fontSize} type="range" id="fontSize" max="26" min="10" step="1" className="w-full h-8 form-check-input accent-pink-600" />
     </div>
     <div className="items-stretch form-check mt-1">
-      <input checked={wrapLines} onChange={() => setWrapLines(!wrapLines)} type="checkbox" id="wordwrap" className="form-check-input" />
+      <input checked={wrapLines} onChange={() => setWrapLines(!wrapLines)} type="checkbox" id="wordwrap" className="form-check-input accent-pink-600" />
       <label htmlFor="wordwrap" className="text-center font-mono pl-2">Wrap Text</label>
     </div>
   </React.Fragment>);
--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -6,7 +6,7 @@ function navbar() {
   const setMenuOpen = useEditorStore.use.setMenuOpen()
   return (
     <React.Fragment>
-      <nav className="relative w-full flex flex-wrap items-center justify-between py-4 bg-purple-800 text-white">
+      <nav className="relative w-full flex flex-wrap items-center justify-between py-4 bg-fuchsia-800 text-white">
         <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
           <div className="container-fluid">
             <a className="font-mono text-center text-xl block" href="/">PASTEBIN(1s)</a>
